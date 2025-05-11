8116d87993869066ceca1605e0a7e6d4d44c56cc
Tanya Hohlova <tanya.hohlova10@mail.ru>
2025-05-11 17:53:45 +0300

    исправление тестов на Cypress

---
+++ cypress/e2e/selectors.js
@@ -0,0 +1,15 @@
+export const Selectors = {
+  INGREDIENT: '[data-testid="ingredient"]',
+  MODAL: '[data-testid="modal"]',
+  ORDER_BUTTON: '[data-testid="order-button"]',
+  MODAL_CLOSE_BUTTON: '[data-testid="modal-close-button"]',
+  MODAL_OVERLAY: '[data-testid="modal-overlay"]',
+  INGREDIENT_DETAILS_NAME: '[data-testid="ingredient-details-name"]',
+  INGREDIENT_DETAILS_CALORIES: '[data-testid="ingredient-details-calories"]',
+  INGREDIENT_DETAILS_PROTEINS: '[data-testid="ingredient-details-proteins"]',
+  INGREDIENT_DETAILS_FAT: '[data-testid="ingredient-details-fat"]',
+  INGREDIENT_DETAILS_CARBOHYDRATES:
+    '[data-testid="ingredient-details-carbohydrates"]',
+  ORDER_MODAL: '[data-testid="order-modal"]',
+  CONSTRUCTOR_ELEMENT: '.constructor-element'
+};
